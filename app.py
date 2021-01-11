ae7cea05ce28fbf6ad690afa318420ebf4735c01
guvenonur <gvenonur@hotmail.com>
2021-01-11 08:13:09 +0300

    Add initial app layout

---
+++ app.py
@@ -0,0 +1,93 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import plotly.express as px
+
+
+# Iris bar figure
+def draw_figure():
+    return html.Div([
+        dbc.Card(
+            dbc.CardBody([
+                dcc.Graph(
+                    figure=px.bar(
+                        df, x="sepal_width", y="sepal_length", color="species"
+                    ).update_layout(
+                        template='plotly_dark',
+                        plot_bgcolor= 'rgba(0, 0, 0, 0)',
+                        paper_bgcolor= 'rgba(0, 0, 0, 0)',
+                    ),
+                    config={
+                        'displayModeBar': False
+                    }
+                )
+            ])
+        ),
+    ])
+
+
+# Text field
+def draw_text():
+    return html.Div([
+        dbc.Card(
+            dbc.CardBody([
+                html.Div([
+                    html.H2("Place holder"),
+                ], style={'textAlign': 'center'})
+            ])
+        ),
+    ])
+
+
+# Data
+df = px.data.iris()
+
+# Build App
+app = dash.Dash(external_stylesheets=[dbc.themes.SLATE])
+
+app.layout = html.Div([
+    dbc.Card(
+        dbc.CardBody([
+            dbc.Row([
+                dbc.Col([
+                    draw_text()
+                ], width=3),
+                dbc.Col([
+                    draw_text()
+                ], width=3),
+                dbc.Col([
+                    draw_text()
+                ], width=3),
+                dbc.Col([
+                    draw_text()
+                ], width=3),
+            ], align='center'),
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    draw_figure()
+                ], width=3),
+                dbc.Col([
+                    draw_figure()
+                ], width=3),
+                dbc.Col([
+                    draw_figure()
+                ], width=6),
+            ], align='center'),
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    draw_figure()
+                ], width=9),
+                dbc.Col([
+                    draw_figure()
+                ], width=3),
+            ], align='center'),
+        ]), color='dark'
+    )
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
